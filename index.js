65b6f2161dcdc02edc20224149542412f6845a8c
bfreed76 <brintonfoyreed@gmail.com>
2021-02-25 13:46:15 -0700

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,56 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(x,y) {
+    return x + y;
+}
+
+add (344, 2);
+
+function subtract(x , y) {
+    return x - y;
+}
+
+subtract (0, 369);
+
+function multiply(a , b) {
+    return a * b;
+}
+
+multiply (3, 4);
+
+function divide(a, b) {
+    return a/b;
+}
+
+divide (3,54);
+
+
+function increment(n) {
+    return n += 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
